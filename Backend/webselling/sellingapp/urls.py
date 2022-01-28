baa25f55b8a3bd00b503d162fcb6b7ecd819b711
Pradip369 <kpuniverse369@gmail.com>
2022-01-28 14:17:04 +0530

    second

--- Backend/webselling/sellingapp/urls.py
+++ Backend/webselling/sellingapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls.conf import path
-from .views import WebInformationView,ManageAuth,LoginView,BiddingUserView,RegisterView
+from .views import WebInformationView, ManageAuth, LoginView, BiddingUserView, RegisterView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -7,9 +7,8 @@ router.register(r'website_manage', WebInformationView)
 router.register(r'bidding_manage', BiddingUserView)
 
 urlpatterns = [
-path('user_login/', LoginView.as_view(),name = 'login'),
-path('user_register/', RegisterView.as_view(),name = 'register'),
-path('manage_auth/', ManageAuth.as_view(),name = 'manage_auth')
+    path('user_login/', LoginView.as_view(), name='login'),
+    path('user_register/', RegisterView.as_view(), name='register'),
+    path('manage_auth/', ManageAuth.as_view(), name='manage_auth')
 ]
-
 urlpatterns += router.urls
